f66dc708116bba244f97d036f2a7a833d0132e2e
the-cynic <145241566+the-cynic@users.noreply.github.com>
2023-09-17 19:59:33 +0000

    added requirements and other minor changes

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # FITX
+Mock Fitness management system using HTML/CSS for frontend and Python/Flask along with MySQL for backend
--- fitx.py
+++ fitx.py
@@ -2,9 +2,17 @@ from flask import Flask, render_template
 
 app=Flask(__name__)
 
-@app.route("/",methods=["GET"])
+@app.route("/")
 def welcome():
   return render_template("landpg.html")
+
+@app.route("/login")
+def login():
+  return render_template("login.html")
+
+@app.route("/signup")
+def signup():
+  return render_template("signup.html")
 
 if(__name__ == "__main__"):
     app.run(host="0.0.0.0",debug=True)
