c97a060482ba344e7c1f2292b77436873781adb9
the-cynic <145241566+the-cynic@users.noreply.github.com>
2023-09-17 16:52:28 +0000

    set up landing page

--- fitx.py
+++ fitx.py
@@ -1,1 +1,10 @@
-print("Hello world")
+from flask import Flask, render_template
+
+app=Flask(__name__)
+
+@app.route("/",methods=["GET"])
+def welcome():
+  return render_template("landpg.html")
+
+if(__name__ == "__main__"):
+    app.run(host="0.0.0.0",debug=True)
