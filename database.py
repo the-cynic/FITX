877b5a4147ff3540a41057bc9a06183667fe7c00
the-cynic <145241566+the-cynic@users.noreply.github.com>
2023-09-21 08:45:47 +0000

    added database connection

--- database.py
+++ database.py
@@ -1,2 +1,9 @@
 import mysql.connector as sl
+con=sl.connect(host='fitx-thecynic-ecc2.aivencloud.com',user='avnadmin',passwd='AVNS_EObxd5cqJt_eLT6DqG9',port=26647,database='defaultdb')
 
+if con.is_connected():
+  cursor=con.cursor()
+  cursor.execute("SHOW TABLES")
+  print(cursor.fetchall())
+  con.commit()
+  con.close()
