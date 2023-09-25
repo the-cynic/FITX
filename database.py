bd05fb806edb4437eb65625acd23ed53b1161043
the-cynic <145241566+the-cynic@users.noreply.github.com>
2023-09-25 05:58:51 +0000

    some changes

--- database.py
+++ database.py
@@ -1,9 +1,17 @@
 import mysql.connector as sl
 con=sl.connect(host='fitx-thecynic-ecc2.aivencloud.com',user='avnadmin',passwd='AVNS_EObxd5cqJt_eLT6DqG9',port=26647,database='defaultdb')
 
-if con.is_connected():
-  cursor=con.cursor()
-  cursor.execute("SHOW TABLES")
-  print(cursor.fetchall())
-  con.commit()
-  con.close()
+def newuser(name,email,passw):
+  if con.is_connected():
+    cursor=con.cursor()
+    cursor.execute("select email from users")
+    lst=cursor.fetchall()
+    for i in lst:
+      if i[0]==email:
+        return False
+    qs="insert into users(name,email,password) values('{}','{}','{}')".format(name,email,passw)
+    cursor.execute(qs)
+    con.commit()
+    con.close()
+    return True
+  return False
--- fitx.py
+++ fitx.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash
+from database import newuser
 
 app=Flask(__name__)
+app.secret_key="ayu~7-@098jir c7^wiug"
 
 @app.route("/")
 def welcome():
@@ -19,7 +21,15 @@ def signup():
     name=str(request.form.get('Name'))
     email=str(request.form.get('Email'))
     passw=str(request.form.get('Passw'))
-    print("Name:",name,"Email:",email,"Password:",passw)
+    if len(name)<3:
+      flash('Name must be greater than 2 characters')
+    elif len(passw)<4:
+      flash('Password must be greater than 4 characters')
+    else:
+      if newuser(name,email,passw):
+        flash('Account created successfully!')
+      else:
+        flash('Email already registered!')
   return render_template("signup.html")
 
 if(__name__ == "__main__"):
